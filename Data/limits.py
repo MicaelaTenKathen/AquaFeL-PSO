96deb2b017eb9b2519c37172afc08ef5d8f88e0f
MicaelaTenKathen <micaela.tenkathen@gmail.com>
2021-09-09 15:49:28 +0200

    Init

--- Data/limits.py
+++ Data/limits.py
@@ -3,69 +3,61 @@ from sys import path
 
 
 class Limits:
-    def __init__(self, g, part, s_n, grid, xs, ys, n_data, part_ant, s_ant, file=0):
-        self.part = part
+    def __init__(self, grid, xs, ys, file=0):
         self.file = file
-        self.s_n = s_n
         self.secure = grid
-        self.n_data = n_data
-        self.part_ant = part_ant
-        self.g = g
-        self.s_ant = s_ant
         self.xs = xs
         self.ys = ys
-        self.x_int = self.part_ant[self.g, 2]
-        self.y_int = self.part_ant[self.g, 3]
         return
 
-    def ratio_s(self):
-        x_int = int(self.x_int)
-        y_int = int(self.y_int)
+    def ratio_s(self, x_int, y_int, part):
+        x_int = int(x_int)
+        y_int = int(y_int)
         x_left = x_int + 2
         x_right = x_int - 2
         y_up = y_int + 2
         y_down = y_int - 2
-        x_i = int(self.part[0])
-        y_i = int(self.part[1])
+        x_i = int(part[0])
+        y_i = int(part[1])
         if self.secure[x_right, y_down] == 1:
-            self.part[0] = x_right
-            self.part[1] = y_down
+            part[0] = x_right
+            part[1] = y_down
         else:
             if self.secure[x_int, y_down] == 1:
-                self.part[1] = y_down
-                self.part[0] = x_int
+                part[1] = y_down
+                part[0] = x_int
             else:
                 if self.secure[x_left, y_i] == 1:
-                    self.part[0] = x_left
-                    self.part[1] = y_int
+                    part[0] = x_left
+                    part[1] = y_int
                 else:
                     if self.secure[x_right, y_i] == 1:
-                        self.part[0] = x_right
-                        self.part[1] = y_int
+                        part[0] = x_right
+                        part[1] = y_int
                     else:
                         if self.secure[x_i, y_up] == 1:
-                            self.part[1] = y_up
-                            self.part[0] = x_int
+                            part[1] = y_up
+                            part[0] = x_int
                         else:
-                            self.part[0] = x_i
-                            self.part[1] = y_i
-        return self.part
+                            part[0] = x_i
+                            part[1] = y_i
+        return part
 
-    def new_limit(self):
+    def new_limit(self, g, part, s_n, n_data, s_ant, part_ant):
         with open(path[-1] + '/Data/bounds.npy'.format(self.file), 'rb') as bn:
             df_bounds = np.load(bn)
-        x_int = int(self.part[0])
-        y_int = int(self.part[1])
-        s_1 = self.s_n[0]
-        s_2 = self.s_n[1]
-        s_3 = self.s_n[2]
-        s_4 = self.s_n[3]
+        x_int = int(part[0])
+        y_int = int(part[1])
+        s_1 = s_n[0]
+        s_2 = s_n[1]
+        s_3 = s_n[2]
+        s_4 = s_n[3]
         if x_int >= self.xs:
-            self.part[0] = self.xs - 1
-            x_int = int(self.part[0])
+            part[0] = self.xs - 1
+            x_int = int(part[0])
         if y_int >= self.ys:
-            self.part[1] = self.ys - 1
-            y_int = (self.part[1])
+            part[1] = self.ys - 1
+            y_int = (part[1])
         if self.secure[x_int, y_int] == 0:
             s, n = 0, 0
             bn = list()
@@ -74,62 +66,63 @@ class Limits:
                     s += 1
                     bn.append(df_bounds[i, :])
             if s == 0:
-                if self.part[1] < df_bounds[0, 2]:
-                    self.part[1] = df_bounds[0, 2] + 2
+                if part[1] < df_bounds[0, 2]:
+                    part[1] = df_bounds[0, 2] + 2
                     for i in range(len(df_bounds)):
-                        if df_bounds[i, 2] == int(self.part[1]):
+                        if df_bounds[i, 2] == int(part[1]):
                             s += 1
                             bn.append(df_bounds[i, :])
                 else:
-                    self.part[1] = df_bounds[-1, 2] - 2
+                    part[1] = df_bounds[-1, 2] - 2
                     for i in range(len(df_bounds)):
-                        if df_bounds[i, 2] == int(self.part[1]):
+                        if df_bounds[i, 2] == int(part[1]):
                             s += 1
                             bn.append(df_bounds[i, :])
             bn = np.array(bn)
-            if self.n_data == 1.0:
-                if self.s_ant[0] > 1 and s_1:
-                    self.part = Limits(self.g, self.part, self.s_n, self.secure, self.xs, self.ys, self.n_data, self.part_ant, self.s_ant).ratio_s()
+            limit = Limits(self.secure, self.xs, self.ys)
+            if n_data == 1.0:
+                if s_ant[0] > 1 and s_1:
+                    part = limit.ratio_s(x_int, y_int, part)
                     s_1 = False
                 else:
-                    if self.part[0] <= bn[0, 0]:
-                        self.part[0] = bn[0, 0] + 2
+                    if part[0] <= bn[0, 0]:
+                        part[0] = bn[0, 0] + 2
                     else:
-                        self.part[0] = bn[0, 1] - 2
-                self.s_ant[0] = s
-            elif self.n_data == 2.0:
-                if self.s_ant[1] > 1 and s_2:
-                    self.part = Limits(self.g, self.part, self.s_n, self.secure, self.xs, self.ys, self.n_data, self.part_ant, self.s_ant).ratio_s()
+                        part[0] = bn[0, 1] - 2
+                s_ant[0] = s
+            elif n_data == 2.0:
+                if s_ant[1] > 1 and s_2:
+                    part = limit.ratio_s(x_int, y_int, part)
                     s_2 = False
                 else:
-                    if self.part[0] <= bn[0, 0]:
-                        self.part[0] = bn[0, 0] + 2
+                    if part[0] <= bn[0, 0]:
+                        part[0] = bn[0, 0] + 2
                     else:
-                        self.part[0] = bn[0, 1] - 2
-                self.s_ant[1] = s
-            elif self.n_data == 3.0:
-                if self.s_ant[2] > 1 and s_3:
-                    self.part = Limits(self.g, self.part, self.s_n, self.secure, self.xs, self.ys, self.n_data, self.part_ant, self.s_ant).ratio_s()
+                        part[0] = bn[0, 1] - 2
+                s_ant[1] = s
+            elif n_data == 3.0:
+                if s_ant[2] > 1 and s_3:
+                    part = limit.ratio_s(x_int, y_int, part)
                     s_3 = False
                 else:
-                    if self.part[0] <= bn[0, 0]:
-                        self.part[0] = bn[0, 0] + 2
+                    if part[0] <= bn[0, 0]:
+                        part[0] = bn[0, 0] + 2
                     else:
-                        self.part[0] = bn[0, 1] - 2
-                self.s_ant[2] = s
-            elif self.n_data == 4.0:
-                if self.s_ant[3] > 1 and s_4:
-                    self.part = Limits(self.g, self.part, self.s_n, self.secure, self.xs, self.ys, self.n_data, self.part_ant, self.s_ant).ratio_s()
+                        part[0] = bn[0, 1] - 2
+                s_ant[2] = s
+            elif n_data == 4.0:
+                if s_ant[3] > 1 and s_4:
+                    part = limit.ratio_s(x_int, y_int, part)
                     s_4 = False
 
                 else:
-                    if self.part[0] <= bn[0, 0]:
-                        self.part[0] = bn[0, 0] + 2
+                    if part[0] <= bn[0, 0]:
+                        part[0] = bn[0, 0] + 2
                     else:
-                        self.part[0] = bn[0, 1] - 2
-                self.s_ant[3] = s
+                        part[0] = bn[0, 1] - 2
+                s_ant[3] = s
         s_n = [s_1, s_2, s_3, s_4]
-        return self.part, s_n
+        return part, s_n
 
 
 
--- PSO/pso_function.py
+++ PSO/pso_function.py
@@ -9,80 +9,79 @@ from deap import creator
 from deap import tools
 
 
-class PSO_init:
-    def __init__(self, g, GEN, c1, c2, c3, c4, pmin, pmax, part, best, sigma_best, mu_best, grid_min, grid_max, population=4, smin=0, smax=0.02, size=2, wmin=0.004, wmax=0.009):
+class PSO:
+    def __init__(self, GEN, grid_min, grid_max, secure, xs, ys, X_test, bench_function, df_bounds):
         self.GEN = GEN
-        self.g = g
-        self.population = population
+        self.population = 4
         self.grid_min = grid_min
         self.grid_max = grid_max
-        self.c1 = c1
-        self.c2 = c2
-        self.c3 = c3
-        self.c4 = c4
-        self.pmin = pmin
-        self.pmax = pmax
-        self.part = part
-        self.best = best
-        self.sigma_best = sigma_best
-        self.mu_best = mu_best
-        self.smin = smin
-        self.smax = smax
-        self.size = size
-        self.wmin = wmin
-        self.wmax = wmax
+        self.pmin = grid_min
+        self.pmax = grid_max
+        self.smin = 0
+        self.smax = 0.02
+        self.size = 2
+        self.wmin = 0.004
+        self.wmax = 0.009
+        self.secure = secure
+        self.xs = xs
+        self.ys = ys
+        self.X_test = X_test
+        self.bench_function = bench_function
+        self.df_bounds = df_bounds
+
+
         return
 
     def initPSO(self):
         creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-        creator.create("Particle", np.ndarray, fitness=creator.FitnessMax, speed=None, smin=None, smax=None, best=None)
+        creator.create("Particle", np.ndarray, fitness=creator.FitnessMax, speed=None, smin=None, smax=None,
+                       best=None)
         creator.create("BestGP", np.ndarray, fitness=creator.FitnessMax)
 
-    def generate(self):
-        part = creator.Particle([random.uniform(self.pmin, self.pmax) for _ in range(self.size)])
-        part.speed = np.array([random.uniform(self.smin, self.smax) for _ in range(self.size)])
-        part.smin = self.smin
-        part.smax = self.smax
+    def generatePart(self):
+        self.part = creator.Particle([random.uniform(self.pmin, self.pmax) for _ in range(self.size)])
+        self.part.speed = np.array([random.uniform(self.smin, self.smax) for _ in range(self.size)])
+        self.part.smin = self.smin
+        self.part.smax = self.smax
+        return self.part
 
-    def updateParticle_n(self):
-        u1 = np.array([random.uniform(0, self.c1) for _ in range(len(self.part))])
-        u2 = np.array([random.uniform(0, self.c2) for _ in range(len(self.part))])
-        u3 = np.array([random.uniform(0, self.c3) for _ in range(len(self.part))])
-        u4 = np.array([random.uniform(0, self.c4) for _ in range(len(self.part))])
-        v_u1 = u1 * (self.part.best - self.part)
-        v_u2 = u2 * (self.best - self.part)
-        v_u3 = u3 * (self.sigma_best - self.part)
-        v_u4 = u4 * (self.mu_best - self.part)
-        w = self.wmax - ((self.wmax - self.wmin) / self.GEN) * self.g
-        self.part.speed = v_u1 + v_u2 + v_u3 + v_u4 + self.part.speed * w
-        for i, speed in enumerate(self.part.speed):
-            if abs(speed) < self.part.smin:
-                self.part.speed[i] = math.copysign(self.part.smin, speed)
-            elif abs(speed) > self.part.smax:
-                self.part.speed[i] = math.copysign(self.part.smax, speed)
-        self.part[:] = self.part + self.part.speed
+    def updateParticle_n(self, g, c1, c2, c3, c4, part, best, sigma_best, mu_best):
+        u1 = np.array([random.uniform(0, c1) for _ in range(len(part))])
+        u2 = np.array([random.uniform(0, c2) for _ in range(len(part))])
+        u3 = np.array([random.uniform(0, c3) for _ in range(len(part))])
+        u4 = np.array([random.uniform(0, c4) for _ in range(len(part))])
+        v_u1 = u1 * (part.best - part)
+        v_u2 = u2 * (best - part)
+        v_u3 = u3 * (sigma_best - part)
+        v_u4 = u4 * (mu_best - part)
+        w = self.wmax - ((self.wmax - self.wmin) / self.GEN) * g
+        part.speed = v_u1 + v_u2 + v_u3 + v_u4 + part.speed * w
+        for i, speed in enumerate(part.speed):
+            if abs(speed) < part.smin:
+                part.speed[i] = math.copysign(part.smin, speed)
+            elif abs(speed) > part.smax:
+                part.speed[i] = math.copysign(part.smax, speed)
+        part[:] = part + part.speed
+        return part
 
     def tool_n(self):
         toolbox = base.Toolbox()
-        toolbox.register("particle", PSO_init(self.g, self.GEN, self.c1, self.c2, self.c3, self.c4, self.pmin, self.pmax,
-                                              self.part, self.best, self.sigma_best, self.mu_best, self.grid_min, self.grid_max).generate())
+        gen = PSO(self.GEN, self.grid_min, self.grid_max, self.secure, self.xs, self.ys, self.X_test, self.bench_function).generatePart
+        toolbox.register("particle", gen)
         toolbox.register("population", tools.initRepeat, list, toolbox.particle)
-        toolbox.register("update", PSO_init(self.g, self.GEN, self.c1, self.c2, self.c3, self.c4, self.pmin, self.pmax,
-                                              self.part, self.best, self.sigma_best, self.mu_best, self.grid_min, self.grid_max).updateParticle_n())
+        toolbox.register("update",
+                         PSO(self.GEN, self.grid_min, self.grid_max, self.secure, self.xs, self.ys, self.X_test, self.bench_function).updateParticle_n)
+        return toolbox
 
     def swarm(self):
-        pop = PSO_init(self.g, self.GEN, self.c1, self.c2, self.c3, self.c4, self.pmin, self.pmax, self.part,
-                 self.best, self.sigma_best, self.mu_best, self.grid_min, self.grid_max).tool_n().population(n=self.population)
+        pop = PSO(self.GEN, self.grid_min, self.grid_max, self.secure, self.xs, self.ys, self.X_test, self.bench_function).tool_n().population(
+            n=self.population)
         best = pop[0]
         return pop, best
 
+    def pso_fitness(self, g, s_n, n_data, part_ant, s_ant, part, ok, x_h, y_h, fitness, x_p, y_p, y_data, init, grid_min, x_g, y_g, n, n_plot, best):
 
-class PSO_fitness:
-    def __init__(self):
-
-        return
-    def pso_fitness(self, s_n, n_data, part_ant, s_ant, part, ok, x_h, y_h, fitness, x_p, y_p, y_data, init, grid_min, x_g, y_g, n, n_plot, best):
-        part, s_n = Limits(self.g, part, s_n, self.secure, self.xs, self.ys, n_data, part_ant, s_ant,
+        part, s_n = Limits(g, part, s_n, self.secure, self.xs, self.ys, n_data, part_ant, s_ant,
                            file=0).new_limit()
         x_bench = int(part[0])
         y_bench = int(part[1])
@@ -110,7 +109,7 @@ class PSO_fitness:
                 part.best = creator.Particle(part)
                 part.best.fitness.values = part.fitness.values
             else:
-                if self.g == self.GEN - 1:
+                if g == self.GEN - 1:
                     x_gap = int(part[0]) + abs(grid_min)
                     y_gap = int(part[1]) + abs(grid_min)
                     x_g.append(x_gap)
@@ -126,4 +125,4 @@ class PSO_fitness:
                 best = creator.Particle(part)
                 best.fitness.values = part.fitness.values
 
-        return
+        return ok, x_h, y_h, fitness, x_p, y_p, y_data, x_bench, y_bench, part, best, n_plot, s_n
