eaa38027e8a3cfa06cc3e4c80761597dc2f5616f
MicaelaTenKathen <micaela.tenkathen@gmail.com>
2022-06-09 10:43:13 +0100

    error

--- Environment/plot.py
+++ Environment/plot.py
@@ -292,7 +292,7 @@ class Plots():
         ticks_y = ticker.FuncFormatter(lambda x, pos: format(int(x * 100), ','))
         axs[1].yaxis.set_major_formatter(ticks_y)
 
-        plt.savefig("../Image/Contamination/GT3/Tabla_3.png")
+        #plt.savefig("../Image/Contamination/GT3/Tabla_3.png")
         plt.show()
 
     def movement_exploration(self, mu, sigma, part_ant):
